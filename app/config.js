373d86bf1f78b0fdf6d5cb16cfa8aa4b61554c37
NBH9 <niamh.bridsonhubbard@thepsc.co.uk>
2020-12-07 10:06:59 +0000

    updating capitalisation

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Space Surveillance & Tracking',
+  serviceName: 'Space surveillance & tracking',
 
   // Default port that prototype runs on
   port: '3001',
