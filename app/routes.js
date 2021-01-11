6f3d4bbfbad98cdbd3eb1d093000a3fdc2475ade
Fiona <fiona.jamieson@thepsc.co>
2021-01-11 17:52:43 +0000

    storing session data

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+req.session.data['data-source']
+req.session.data['contact']
+
 
 module.exports = router
