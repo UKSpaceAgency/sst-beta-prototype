87605dec00df1a5331ab472a0f40cb93f3016046
Fiona <fiona.jamieson@thepsc.co>
2021-02-11 15:45:04 +0000

    adding filter to test page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,4 +8,21 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
     window.MOJFrontend.initAll()
+    new MOJFrontend.FilterToggleButton({
+  bigModeMediaQuery: '(min-width: 48.063em)',
+  startHidden: true,
+  toggleButton: {
+    container: $('.moj-action-bar__filter'),
+    showText: 'Show filter',
+    hideText: 'Hide filter',
+    classes: 'govuk-button--secondary'
+  },
+  closeButton: {
+    container: $('.moj-filter__header-action'),
+    text: 'Close'
+  },
+  filter: {
+    container: $('.moj-filter')
+  }
+});
 })
