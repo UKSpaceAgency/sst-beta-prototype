4b69c0e21d48201b9e320d4b89ac543b58aaa296
Sophie-Cooper-PSC <78089306+Sophie-Cooper-PSC@users.noreply.github.com>
2021-10-11 15:43:26 +0100

    Revert "Satellite table standardisation + log in pages"

--- app/routes.js
+++ app/routes.js
@@ -3,20 +3,4 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-// Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/login-email-answer', function (req, res) {
-
-  // Make a variable and give it the value from 'how-many-balls'
-  var email-address = req.session.data['email-address']
-
-  // Check whether the variable matches a condition
-  if (email-address == "thepsc@space.co.uk"){
-    // Send user to next page
-    res.redirect('/sucessful-login')
-  } else {
-    // Send user to ineligible page
-    res.redirect('/invalid-email')
-  }
-
-})
 module.exports = router
