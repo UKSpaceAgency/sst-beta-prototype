e417a37aee269572a2c7449745d09091d22751a9
Henry Wright <105492467+henrywrightPSC@users.noreply.github.com>
2023-12-12 15:41:12 +0000

    [SS-1982] Manually migrated prototype kit to v13 from v12 (#321)
    * Manually migrated prototype kit to v13 from v12
    * Added main layout changes in to allow for this

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,28 +1,25 @@
-/* global $ */
+//
+// For guidance on how to add JavaScript see:
+// https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
+//
 
-// Warn about using the kit in production
-if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
-}
-
-$(document).ready(function () {
-  window.GOVUKFrontend.initAll()
-    window.MOJFrontend.initAll()
+window.GOVUKPrototypeKit.documentReady(() => {
+    window.MOJFrontend.initAll();
     new MOJFrontend.FilterToggleButton({
-  bigModeMediaQuery: '(min-width: 48.063em)',
-  startHidden: true,
-  toggleButton: {
-    container: $('.moj-action-bar__filter'),
-    showText: 'Filter by data source',
-    hideText: 'Hide filter',
-    classes: 'govuk-button--secondary'
-  },
-  closeButton: {
-    container: $('.moj-filter__header-action'),
-    text: 'Close'
-  },
-  filter: {
-    container: $('.moj-filter')
-  }
-});
+      bigModeMediaQuery: '(min-width: 48.063em)',
+      startHidden: true,
+      toggleButton: {
+        container: $('.moj-action-bar__filter'),
+        showText: 'Filter by data source',
+        hideText: 'Hide filter',
+        classes: 'govuk-button--secondary'
+      },
+      closeButton: {
+        container: $('.moj-filter__header-action'),
+        text: 'Close'
+      },
+      filter: {
+        container: $('.moj-filter')
+      }
+      });
 })
