bd083b06af1da350e43114e9ada7b487daf42f73
matthew-traylenpsc <matthew.traylen@thepsc.co.uk>
2024-07-25 11:50:52 +0100

    branching

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,30 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/account/superadmin/invite-new-member', function(request, response) {
+
+    var userrole = request.session.data['userrole']
+    if (userrole == "government-admin"){
+        response.redirect("/account/superadmin/choose-alert-settings")
+    } if (userrole == "government-user"){
+        response.redirect("/account/superadmin/choose-alert-settings")
+    }if (userrole == "agency-user"){
+        response.redirect("/account/superadmin/choose-alert-settings")
+    }
+    if (userrole == "agency-admin"){
+        response.redirect("/account/superadmin/choose-alert-settings")
+    }
+    if (userrole == "agency-analyst"){
+        response.redirect("/account/superadmin/choose-alert-settings")
+    }
+    if (userrole == "agency-approver"){
+        response.redirect("/account/superadmin/choose-alert-settings")
+    }
+    if (userrole == "agency-superuser"){
+        response.redirect("/account/superadmin/choose-alert-settings")
+    } else {
+        response.redirect("/account/superadmin/check-user-details2")
+    }
+})
+
